b71cfadd1cda94c270062c0f7e3f55158647e615
Walter Vogel <wvogel@wgmv.de>
2020-01-30 17:30:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Excercise from Oxid Academy
+
+* Excercise 9.1:
+    * https://www.udemy.com/course/oxid-eshop-entwicklerschulung-basis/learn/lecture/17215052?start=600
---
+++ metadata.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'oawidgettest',
+    'title'       => [
+        'de' => 'Widget Test',
+        'en' => 'Widget Test',
+    ],
+    'description' => [
+        'de' => 'Das Modul testet die Widget Funktionalitaet',
+        'en' => 'This module tests the widget functionality',
+    ],
+    'thumbnail'   => '',
+    'version'     => '1.0',
+    'author'      => 'wgmv',
+    'url'         => '',
+    'email'       => '',
+    'extend'      => [
+
+    ],
+    'blocks'      => [
+
+    ],
+    'settings'    => [
+
+    ],
+    'events'      => [
+    ],
+];
